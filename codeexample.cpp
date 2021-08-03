57a373d179b29e944fcb3806ef4ee6ecf420ea15
Piotr <eu.piotr@gmail.com>
2021-08-03 16:59:36 +0200

    update

--- codeexample.cpp
+++ codeexample.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 
+<<<<<<< HEAD
 int main_function() {
 	// do everything in main() here!
+=======
+void branch_function(int x, std::string s) {
+	// do nothing on branch
+>>>>>>> 763b589f365987e105ded68f809c07e6cfe52232
 }
 
 int main() {
