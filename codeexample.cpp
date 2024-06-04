e4f1b28bce0508fa3fbe28dc670fa7052d385e85
Piotr Cieslak <eu.piotr@gmail.com>
2024-06-04 12:35:14 +0200

    new commit

---
+++ codeexample.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main_function() {
+	// do everything in main() here!
+void branch_function(int x, std::string s) {
+	// do nothing on branch
+}
+
+int main() {
+	return 0;
+}
